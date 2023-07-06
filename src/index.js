5adee1d7816b790b35f6fded801746419ba7c6c5
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-07-06 17:46:47 -0600

    Add clear function and tab navigation logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-import { loadPage } from './page-load';
+import { loadHomePageContent, loadPage } from './page-load';
+import { showMenuPage } from './menu-page.js';
 import './style.css';
 
-console.log('im alive!');
 loadPage();
+
+function clearPage () {
+    const pageContent = document.querySelector('article');
+
+    while (pageContent.firstChild) {
+        pageContent.removeChild(pageContent.firstChild);
+    }
+}
+
+const homeBtn = document.querySelector('.home-button');
+homeBtn.addEventListener('click', () => {
+    clearPage();
+    loadHomePageContent();
+})
+
+const menuBtn = document.querySelector('.menu-button');
+menuBtn.addEventListener('click', () => {
+    clearPage();
+    showMenuPage();
+});
+
