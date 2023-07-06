4644ea2759ad3e1bc299917b0140b0b340ca3610
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-07-06 17:48:19 -0600

    Add more depth and structure for easy access
    Also added loadHomePageContent and exported it so that the
    'article' element would only be created once, and we could
    re-load the home page at will

--- src/page-load.js
+++ src/page-load.js
@@ -1,33 +1,60 @@
 const container = document.querySelector('#content');
-import icon from './pasta-mushroom-food-dinner-italian-wallpaper.jpg';
 
 function loadTitle () {
     const title = document.createElement('div');
     title.classList.add('page-title');
-    title.textContent = 'Allegro! Fine Pasta';
+
+    const titleHeader = document.createElement('h1');
+    titleHeader.textContent = 'Allegro! Fine Pasta';
+
+    title.appendChild(titleHeader);
+
     container.appendChild(title);
 }
 
 function loadNav () {
     const navBar = document.createElement('div');
     navBar.classList.add('nav');
-    navBar.textContent = 'home | menu | about';
+
+    const homeBtn = document.createElement('button');
+    homeBtn.classList.add('home-button');
+    homeBtn.textContent = 'home';
+    navBar.appendChild(homeBtn);
+
+    const menuBtn = document.createElement('button');
+    menuBtn.classList.add('menu-button');
+    menuBtn.textContent = 'menu';
+    navBar.appendChild(menuBtn);
+
+    const aboutBtn = document.createElement('button');
+    aboutBtn.textContent = 'about';
+    navBar.appendChild(aboutBtn);
+
     container.appendChild(navBar);
 }
 
 function loadPageBody () {
     const pageBody = document.createElement('article');
-    pageBody.textContent = 'lorem ipsum etc. etc.';
     container.appendChild(pageBody);
+}
+
+function loadHomePageContent () {
+    const pageBody = document.querySelector('article');
+    const homeContent = document.createElement('div');
+    homeContent.classList.add('home-content');
+    homeContent.textContent = 'Do you want pasta? Who are you kidding - we both know you do';
+    pageBody.appendChild(homeContent);
 }
 
 function loadPage () {
     loadTitle();
     loadNav();
     loadPageBody();
+    loadHomePageContent();
 }
 
 export {
     loadPage,
+    loadHomePageContent,
 };
 
